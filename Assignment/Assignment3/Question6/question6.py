8bd180a0fedf494cc35407e8bcc505d387b6e1eb
Nirajan Sah <Nirajan2.Sah@live.uwe.ac.uk>
2025-03-28 23:50:37 +0545

    add new files

---
+++ Assignment/Assignment3/Question6/question6.py
@@ -0,0 +1,26 @@
+class Student():
+    def __init__(self, id, name, address, year, lvl, sec):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.year = year
+        self.level = lvl
+        self.section = sec
+
+    def giveOutput(self):
+        print(f"Name: {self.name}")
+        print(f"Id: {self.id}")
+        print(f"address: {self.address}")
+        print(f"year: {self.year}")
+        print(f"level: {self.level}")
+        print(f"Section: {self.section}")
+
+
+id = input("Enter your ID: ")
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+year = input("Enter your year: ")
+level = input("Enter your level: ")
+section = input("Enter your section: ")
+s1 = Student(id, name, address, year, level, section)
+s1.giveOutput()
