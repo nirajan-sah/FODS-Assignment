fb04a2e33b5beb80fee3021c80b05360b52596c5
Nirajan Sah <Nirajan2.Sah@live.uwe.ac.uk>
2025-04-14 17:23:16 +0545

    Modify

--- Assignment/Assignment1/Part_2/question_1.py
+++ Assignment/Assignment1/Part_2/question_1.py
@@ -3,7 +3,7 @@ Program to take a number as input and display if it is even or odd
 '''
 
 #prompt user to input number
-a = int(input("Enter a number"))
+a = int(input("Enter a number: "))
 
 #use statement if the number returns any remainder when divided by 2
 if a % 2 == 0:
