b077449874758f082359bcf292f1f49f7f446152
Nirajan Sah <Nirajan2.Sah@live.uwe.ac.uk>
2025-04-14 21:25:47 +0545

    modify

--- Assignment/Assignment4/question7.py
+++ Assignment/Assignment4/question7.py
@@ -6,12 +6,12 @@ import numpy as np
 
 #Creating a random matrix
 arr = np.random.rand(1*10)
-print(arr)
+print("The 1 * 10 matrix is ",arr)
 
 #Reshaping it into 2 rows and 5 columns
 re_arr = arr.reshape(2, 5)
-print(re_arr)
+print("The 2 * 5 matrix is ",re_arr)
 
 #Reshaping it into 5 rows and 2 columns
 re_arr_ = arr.reshape(5, 2)
-print(re_arr_)
+print("The 5 * 2 matrix is ",re_arr_)
